eb0db57fe8cb97ee392e2249e52ff4628b346018
jieun <forworkje21@gmail.com>
2024-07-20 11:07:34 +0900

    Feat Webpack Dev 서버 실행

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'none',
-    entry: './src/index.js',
+    entry: './index.js',
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    }
-};
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist'),
+    },
+    devServer: {
+        port: 3000
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            // index.html 템플릿을 기반으로 빌드 결과물을 추가
+            template: './index.html',
+        })
+    ]
+}
